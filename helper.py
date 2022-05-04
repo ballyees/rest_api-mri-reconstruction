6886a0fc21cea0db4efc6d5f34a33ccd1dafc090
ballyees <shouldertip@gmail.com>
2022-05-04 18:38:29 +0700

    edit preprocessing

--- helper.py
+++ helper.py
@@ -18,8 +18,10 @@ def read_image_base64(image):
     image = Image.open(BytesIO(image)).convert('RGB')
     image = np.asarray(image, dtype=np.uint8)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) 
-    image = np.expand_dims(image, 0)
-    return image / 255
+    image = cv2.resize(image, (320, 640)) 
+    image = cv2.GaussianBlur(image, (3,3), 0)
+    image = np.expand_dims(image, 0) / 255
+    return image
 
 def round_image(image_norm):
     image_norm = np.round(image_norm * 255)
@@ -27,7 +29,6 @@ def round_image(image_norm):
 
 def image_to_json(image):
     image = np.squeeze(image)
-    image = cv2.GaussianBlur(image, (3,3),cv2.BORDER_DEFAULT)
     image = round_image(image)
     _, img_bytes = cv2.imencode('.png', image)
     img_bytes = base64.b64encode(img_bytes)
